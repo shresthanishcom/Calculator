39501a8188d40f2e4ca36405d9e49e0fd0a0def4
shresthanishcom <me.anish.shrestha@gmail.com>
2021-12-14 15:13:00 +0545

    now calculator

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+function listIcon() {
+  if (document.querySelector(".Nav-dropdown").style.display === "block") {
+    document.querySelector("i").style = "transform:rotate(0deg);";
+    document.querySelector(".Nav-dropdown").style = "display:none;";
+  } else {
+    document.querySelector("i").style = "transform:rotate(90deg);";
+    document.querySelector(".Nav-dropdown").style =
+      "display:block;background-color:black;position:fixed;width:100%";
+  }
+}
